0c4e0bfdca90d55bca2347de471d88e39ec3f643
dev <dev@devs-MacBook-Pro.local>
2017-02-20 22:36:51 +0100

    new branch

--- testers/src/main/java/com/testers/Mainer.java
+++ testers/src/main/java/com/testers/Mainer.java
@@ -4,9 +4,7 @@ public class Mainer {
 
 	public static void main(String[] args)
 	{
-		System.out.println("Now we testing1");
-		System.out.println("Now we testing2");
-		System.out.println("Now we pull request");
+		System.out.println("New brach");
 
 	}
 
